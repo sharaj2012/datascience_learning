f5dfb0537d6d76569e01da32b9150ade3b6b06d5
sraj2012 <sraj@oxfordproperties.com>
2021-04-05 15:23:39 +0530

    added files

--- machine_learning/machine_learninf.py
+++ machine_learning/machine_learninf.py
@@ -2,22 +2,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
 # %%
 dataset =  pd.read_csv('Data.csv')
+
 # %%
 dataset
+
 # %%
 x = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,-1].values
-# %%
-# Taking care of missing data
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
-imputer.fit(x[:,1:3]) ## to connect the object to the matrix of features x 
-x[:,1:3] = imputer.transform(x[:,1:3]) ## apply the transformation
+y = dataset.iloc[:,-1].values 
 
 #%%
 print(x)
+
+# %%   
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
+#imputer.fit(x[:,1:3]) # to connect the object to the matrix of features x 
+x[:,1:3] = imputer.fit_transform(x[:,1:3]) # apply the transformation
+
+#%%
+print(x)
+
 # %%
 # Encoding the independent variable
 # using One Hot Encoding we turn county column into 3 columns, in this example France will have code 100,Spain = 010, Germany= 001, so there is no numerical order between thises countries, so ML model wont find any relation between these crea
@@ -32,18 +40,22 @@ x = np.array(ct.fit_transform(x))
 
 # %%
 print(x)
+
 # %%
 ## Encoding the Dependent Variable
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
+
 # %%
 print(y)
+
 # %%
 lex = LabelEncoder()
 lex.fit(["amsterdam", "paris","tokyo", "tokyo"])
 lex.transform(["amsterdam", "tokyo", "tokyo"])
+
 # %%
 # splitting dataset into Training and Test Set
 # Feature Scaling - Do we need to apply feature scaling before or after splitting the dataset--------Ans: Feature scaling sud be done after splitting the dataset, because we sud not apply feature scaling on the test set since its suppose to be something of a new observation
@@ -57,7 +69,7 @@ lex.transform(["amsterdam", "tokyo", "tokyo"])
 # x_train & y_train are expected in a method called the fit method and for the predictions also called as Inference
 
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2, random_state = 1)
+x_train,x_test,y_train,y_test = train_test_split(x,y ,test_size = 0.2, random_state = 1)
 # x,y = independent,dependent variable
 # test_size = how much % of dataset as Test Set
 # random_state = 1, so that the test_set and traing_Set gets randomly created
@@ -67,6 +79,20 @@ print(x_test)
 print(y_train)
 print(y_test)
 
+# %%
+# how are we going to apply feature scaling tenchiques -> Standardisation | Normalisation
+# standardisation work all the time
+# Q. do we have to apply feature scaling to the dummy variables in the matrix of features i.e "1.0 0.0 0.0" in [1.0 0.0 0.0 44.0 72000.0].....Ans. No since they already between 0 & 1, and if we apply standardisation,we will get some rubbish value which wont help
 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:,3:] = sc.fit_transform(x_train[:,3:])
+x_test[:,3:] = sc.transform(x_test[:,3:]) # here we using standardisation we did on x_train and applied it on x_test,we didnot create a new sdandardisation
 
 # %%
+print(x_train)
+
+# %%
+print(x_test)
+# %%
+ 
---
+++ machine_learning/simple_linear_regression.py
@@ -0,0 +1,58 @@
+#%%
+# importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.utils.validation import check_random_state
+
+# %%
+# importing the dataset
+salary_dataset = pd.read_csv('Salary_Data.csv')
+
+# %%
+salary_dataset
+
+# %%
+x = salary_dataset.iloc[:,:-1].values
+y = salary_dataset.iloc[:,-1].values
+
+# %%
+y
+
+# %%
+#splitting the dataset into training set and test set 
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=.2,random_state=0) 
+
+# %%
+print(x_train)
+ 
+ # %%
+ # Training the simple Linear Regression model on the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+# %%
+# predicting the Test set results
+y_pred = regressor.predict(x_test)
+
+# %%
+# visualizing the training set results
+plt.scatter(x_train,y_train,color = 'red')
+plt.plot(x_train,regressor.predict(x_train),color='blue')
+plt.title('Salary vs Experience(Training Set')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# %%
+# visualizing the test set result
+plt.scatter(x_test,y_test,color = 'red')
+plt.plot(x_train,regressor.predict(x_train),color='blue')
+plt.title('Salary vs Experience(Test Set')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+# %%
+# we are having such a beautiful graph because the dataset values are in linear relationship
